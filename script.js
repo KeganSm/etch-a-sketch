919c70d3a4f9a3161e12c4a6dff93f312cecd420
Kegan Smith <kegansmith415@gmail.com>
2022-03-22 23:58:42 -1000

    clear button now works without throwing an error

--- script.js
+++ script.js
@@ -20,10 +20,7 @@ container.addEventListener('mouseover', (e)=>{ //adds 'hovered' class to targete
 })
 
 clearBtn.addEventListener('click', ()=>{ //clears grid with button click ...not yet working
-    let cells = document.getElementsByClassName('cell hovered');
-    cells.className = 'cell';
+    let cells = document.querySelectorAll('.hovered');
+    cells.forEach(cell => cell.classList.remove('hovered')); //forEach iterates through nodelist returned by querySelectorAll
 });
 
-//Currently working on a way to figure out to to make the 'hovered' class a seperate class for each cell,
-// bc currrently the cell class becomes 'cell hovered' whenever it is hovered and I haven't figured out a
-//way to make the clear button work to remove the 'hovered' class from the cell
